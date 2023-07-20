e6275c222cd214f03705e513a66a1ee19adb1f0c
ellehailu <hailu.elle@gmail.com>
2023-07-20 13:27:27 -0700

    add a new categories folder in controllers, views, and models

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        [HttpGet("/categories")]
+        public ActionResult Index()
+        {
+            List<Category> allCategories = Category.GetAll();
+            return View(allCategories);
+        }
+
+        [HttpGet("/categories/new")]
+        public ActionResult New(){
+            return View();
+        }
+    }
+}
